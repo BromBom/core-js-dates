64e4b46015015eaaf6833f382606db8511398a32
Ihar Levachkov <pokerlis@gmail.com>
2024-03-10 14:05:10 +0300

    feat: add solution to getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -104,8 +104,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const msResult = new Date(dateEnd).getTime() - new Date(dateStart).getTime();
+  const days = Math.ceil(msResult / 86_400_000) + 1;
+
+  return days;
 }
 
 /**
